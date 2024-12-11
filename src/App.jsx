5255058fbd471bbfd67da6afda3e04ff42d2dd82
eduardorocco <eduardo_rocco@hotmail.it>
2024-12-11 16:19:00 +0100

    REFACTOR PostContext

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,23 @@ import AboutUs from './pages/AboutUs'
 import DefaultLayout from './layout/DefaultLayout'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PostDetail from './pages/PostDetail'
-import { PostProvider } from "./context/PostContext";
+
+
 function App() {
   return (
-    <PostProvider>
+
+ 
       <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout/>}>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/about-us' element={<AboutUs />} />
-          <Route path='/our-recipes' element={<PostsPage/>} />
-          <Route path='/our-recipes/:postId' element={<PostDetail/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </PostProvider>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/about-us' element={<AboutUs />} />
+            <Route path='/our-recipes' element={<PostsPage />} />
+            <Route path='/our-recipes/:postId' element={<PostDetail />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
 
   )
 
